33d9b1dc08804d0225154380f68a1b86a64d755d
reza farazi <rezafarazi1378@gmail.com>
2023-06-20 09:51:22 +0330

    Get push

---
+++ TestClientSocket/TestClientSocket/Program.cs
@@ -0,0 +1,20 @@
+﻿//libs
+using System.Net;
+using System.Net.Sockets;
+
+//get start program
+main();
+
+//main function
+void main() 
+{
+
+    Socket socket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+    socket.Connect(new IPEndPoint(IPAddress.Loopback,8082));
+
+    socket.Send(System.Text.ASCIIEncoding.ASCII.GetBytes("Hello world"));
+
+    socket.Close();
+
+
+}
